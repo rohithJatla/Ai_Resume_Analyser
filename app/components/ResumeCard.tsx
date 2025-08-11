5e91b4d74907b5edadc238a240164b6cd3164939
jatlar1 <jatlar1@udayton.edu>
2025-08-11 18:23:13 -0400

    added home page and auth pages need to implement the authentication logic with puter.

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {resumes} from "~/constants";
+import {Link} from "react-router";
+import ScoreCircle from "~/components/ScoreCircle";
+
+const ResumeCard = ({resume:{id,companyName,jobTitle,feedback,imagePath}}:{resume:Resume}) => {
+    return (
+        <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
+            <div className="resume-card-header">
+                <div className="flex flex-col gap-2">
+                    <h2 className=" !text-black font-bold break-words" >{companyName}</h2>
+                    <h3 className="text-lg text-gray-500 break-words">{jobTitle}</h3>
+                </div>
+                <div className="flex-shrink-0">
+                    <ScoreCircle score={feedback.overallScore}></ScoreCircle>
+                </div>
+            </div>
+            <div className="gradient-border animate-in fade-in duration-1000">
+                <div className="w-full h-full">
+                    <img src={imagePath}
+                        alt="resume"
+                         className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+                    />
+                </div>
+            </div>
+        </Link>
+    )
+}
+export default ResumeCard
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,56 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
+import {usePuterStore} from "~/Lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resly" },
+    { name: "description", content: "Free tool to analyze your resume! to land your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+
+
+
+
+    const {isLoading, auth} = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!auth.isAuthenticated){
+            navigate("/auth?next=/");
+        }
+    },[auth.isAuthenticated])
+
+
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar></Navbar>
+
+      <section className="main-section">
+          <div className="page-heading py-16">
+              <h1> Track your Applications & Resume Ratings</h1>
+              <h2>Review your submissions and check Ai-powered feedback.</h2>
+          </div>
+
+          {resumes.length > 0 && (
+              <div className="resumes-section">
+                  {resumes.map((resume) => (
+                      <ResumeCard key={resume.id} resume={resume} />
+                  ))
+                  }
+              </div>
+          )}
+
+      </section>
+
+
+
+
+  </main>;
 }
